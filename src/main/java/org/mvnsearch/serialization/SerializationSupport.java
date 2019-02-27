fabf8e8746733368fc605d9a8eb80c82918fa219
linux_china <linux_china@hotmail.com>
2019-02-27 12:19:32 -0800

    Serialization support interface

---
+++ src/main/java/org/mvnsearch/serialization/SerializationSupport.java
@@ -0,0 +1,15 @@
+package org.mvnsearch.serialization;
+
+import java.io.IOException;
+
+/**
+ * Serialization support interface
+ *
+ * @author leijuan
+ */
+public interface SerializationSupport<T> {
+
+    byte[] writeObject(T object) throws IOException;
+
+    T readObject(byte[] content) throws IOException;
+}
