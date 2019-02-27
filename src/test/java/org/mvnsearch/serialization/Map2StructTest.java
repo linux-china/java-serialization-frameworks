c42238ec6e586dabdd6fe4be316b6106760f55da
linux_china <linux_china@hotmail.com>
2019-02-27 13:54:34 -0800

    Map2Struct test

---
+++ src/test/java/org/mvnsearch/serialization/Map2StructTest.java
@@ -0,0 +1,19 @@
+package org.mvnsearch.serialization;
+
+import org.junit.jupiter.api.Test;
+import org.mvnsearch.serialization.proto.UserPB;
+
+/**
+ * Map2Struct test
+ *
+ * @author linux_china
+ */
+public class Map2StructTest extends BaseTestCase {
+
+    @Test
+    public void testProtobufAndJava() {
+        User user = constructUser();
+        UserPB userPB = UserMapper.INSTANCE.javaToProtobuf(user);
+        System.out.println(userPB.getBalance());
+    }
+}
