f1c24800a11415f7f896714b73cd58a6c7ca930c
linux_china <linux_china@hotmail.com>
2019-02-27 14:52:10 -0800

    add null check

--- src/main/java/org/mvnsearch/serialization/UserMapper.java
+++ src/main/java/org/mvnsearch/serialization/UserMapper.java
@@ -15,16 +15,16 @@ import org.mvnsearch.serialization.proto.UserPB;
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-    @Mapping(target = "bornAt", expression = "java(new java.util.Date(s.getBornAt()))")
+    @Mapping(target = "bornAt", expression = "java(s.getBornAt()==0?null:new java.util.Date(s.getBornAt()))")
     User convertToJava(UserPB s);
 
-    @Mapping(target = "bornAt", expression = "java(s.getBornAt().getTime())")
+    @Mapping(target = "bornAt", expression = "java(s.getBornAt()==null?0:s.getBornAt().getTime())")
     UserPB convertToProtobuf(User s);
 
-    @Mapping(target = "bornAt", expression = "java(s.getBornAt().getTime())")
+    @Mapping(target = "bornAt", expression = "java(s.getBornAt()==null?0:s.getBornAt().getTime())")
     UserPB convertToProtobuf(UserDTO s);
 
-    @Mapping(target = "bornAt", expression = "java(new java.util.Date(s.getBornAt()))")
+    @Mapping(target = "bornAt", expression = "java(s.getBornAt()==0?null:new java.util.Date(s.getBornAt()))")
     UserDTO convertToDTO(UserPB s);
 
     UserDTO convertToDTO(User s);
