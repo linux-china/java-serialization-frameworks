04207180b7b52a2493db5670a3240fdc754fbe7b
linux_china <libing.chen@gmail.com>
2019-08-05 21:18:17 -0700

    Minor update

--- src/test/java/org/mvnsearch/serialization/AvroTest.java
+++ src/test/java/org/mvnsearch/serialization/AvroTest.java
@@ -3,7 +3,10 @@ package org.mvnsearch.serialization;
 import org.junit.jupiter.api.Test;
 import org.mvnsearch.avro.User;
 
+import java.nio.ByteBuffer;
 import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Apache Avro test
@@ -13,14 +16,17 @@ import java.time.Instant;
 public class AvroTest {
 
     @Test
-    public void testAvroOperations() {
+    public void testAvroOperations() throws Exception {
+        String nick = "linux_china";
         User user = User.newBuilder()
                 .setId(1)
-                .setNick("linux_china")
+                .setNick(nick)
                 .setEmail("linux_china@hotmail.com")
                 .setPhone("188")
                 .setBornAt(Instant.now())
                 .build();
-
+        ByteBuffer byteBuffer = user.toByteBuffer();
+        User user2 = User.fromByteBuffer(byteBuffer);
+        assertThat(user2.getNick()).isEqualTo(nick);
     }
 }
