d08ef1a24acad6046b2035abac95bee9aed393b8
linux_china <libing.chen@gmail.com>
2020-03-16 10:39:56 -0700

    Hessian test

---
+++ src/test/java/org/mvnsearch/serialization/HessianTest.java
@@ -0,0 +1,30 @@
+package org.mvnsearch.serialization;
+
+import com.caucho.hessian.io.HessianSerializerInput;
+import com.caucho.hessian.io.HessianSerializerOutput;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Hessian test
+ *
+ * @author linux_china
+ */
+public class HessianTest extends BaseTestCase {
+
+    @Test
+    public void testOperator() throws Exception {
+        User user = constructUser();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        HessianSerializerOutput output = new HessianSerializerOutput(bos);
+        output.writeObject(user);
+        output.flush();
+        ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+        HessianSerializerInput input = new HessianSerializerInput(bis);
+        User user2 = (User) input.readObject();
+        Assertions.assertThat(user).isEqualToComparingFieldByField(user2);
+    }
+}
