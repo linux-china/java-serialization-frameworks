6e246771bf534b821e081aa042e7b91ca88f394b
linux_china <libing.chen@gmail.com>
2020-05-16 21:10:04 -0700

    add testGenerate

--- src/test/java/org/mvnsearch/serialization/AvroTest.java
+++ src/test/java/org/mvnsearch/serialization/AvroTest.java
@@ -1,8 +1,16 @@
 package org.mvnsearch.serialization;
 
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.EncoderFactory;
 import org.junit.jupiter.api.Test;
 import org.mvnsearch.avro.User;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.nio.ByteBuffer;
 import java.time.Instant;
 
@@ -28,5 +36,22 @@ public class AvroTest {
         ByteBuffer byteBuffer = user.toByteBuffer();
         User user2 = User.fromByteBuffer(byteBuffer);
         assertThat(user2.getNick()).isEqualTo(nick);
+    }
+
+    @Test
+    public void testGenerate() throws Exception {
+        Schema schema = new Schema.Parser().parse(new File("src/main/avro/user.avsc"));
+        GenericRecord user1 = new GenericData.Record(schema);
+        user1.put("id", 1L);
+        user1.put("nick", "linux_china");
+        user1.put("email", "linux_china@hotmail.com");
+        user1.put("bornAt", System.currentTimeMillis());
+        user1.put("balance", 0.2);
+        user1.put("flag", 1L);
+        user1.put("vip", true);
+        DatumWriter<GenericRecord> dataWriter = new GenericDatumWriter<>(schema);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        dataWriter.write(user1, EncoderFactory.get().directBinaryEncoder(bos, null));
+        System.out.println(bos.toByteArray().length);
     }
 }
